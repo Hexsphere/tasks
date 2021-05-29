0d329f434cc90adecee3d8614d048565b4dd2f7e
Ethan Anderson <19194252+Hexsphere@users.noreply.github.com>
2021-05-29 00:59:07 -0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+// Angular Modules
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
-import { TaskComponent } from './task/task.component';
+
+// Third-party Modules
+
+import { NgScrollbarModule } from 'ngx-scrollbar';
+
+// Global Components
 
 import { AppComponent } from './app.component';
+import { ModalComponent } from './modal/modal.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+
+// Page Components
+
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
+
+// Components
+
 import { ProjectComponent } from './project/project.component';
-import { NgScrollbarModule } from 'ngx-scrollbar';
-import { ModalComponent } from './modal/modal.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
